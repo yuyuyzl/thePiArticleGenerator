f3a86da5e268c3472edbc797786dec810ba2abf0
yuyuyzl <yuzhongliang22@163.COM>
2017-08-06 14:17:59 +0800

    固定图片合成器完成

--- js/appPicGen.js
+++ js/appPicGen.js
@@ -9,33 +9,12 @@
     app.controller("mainController",['$sce','$q','$scope','$timeout','$filter',function($sce,$q,$scope,$timeout,$filter){
         this.authorName="良岳";
         this.authorTxt="";
+        this.coverTxt="";
         this.doRefresh=function () {
-
-            var c=document.getElementById("cvsMp");
-            var ctx=c.getContext("2d");
-            ctx.clearRect(0,0,c.width,c.height);
-            var img=document.getElementById("myimg");
-            var scaleN=Math.min(img.width,img.height)/232;
-            if(scaleN!=0){
-
-                console.log(scaleN);
-
-                ctx.scale(1/scaleN,1/scaleN);
-                ctx.drawImage(img,255*scaleN-img.width/2,282*scaleN-img.height/2);
-
-                ctx.scale(scaleN,scaleN);
-            }
-            var img=document.getElementById("imgMpTemp");
-            ctx.drawImage(img,0,0);
-            ctx.fillStyle='#03125e';
-            ctx.font="56px 'Senty Golden Bell 新蒂金钟体'";
-            ctx.fillText(this.authorName,498,239);
-            ctx.fillStyle='#92969c';
-            ctx.font="bold 30px '宋体'";
-            ctx.fontStyle="bold";
-            var txtLs=this.authorTxt.split("\n");
-            for(var i=0;i<txtLs.length;i++)
-                ctx.fillText(txtLs[i],505,340+56*i);
+            authorName=this.authorName;
+            authorTxt=this.authorTxt;
+            coverTxt=this.coverTxt;
+            doRefresh();
         }
     }]);
 })();
@@ -44,13 +23,13 @@
 function Id(id){
     return document.getElementById(id);
 }
-function changeToop(){
-    var file = Id("file");
+function changeToop(fid,iid){
+    var file = Id(fid);
     if(file.value==''){
         //设置默认图片
-        Id("myimg").src='';
+        Id(iid).src='';
     }else{
-        preImg("file","myimg");
+        preImg(fid,iid);
     }
 }
 //获取input[file]图片的url Important
@@ -71,4 +50,67 @@ function getFileUrl(fileId) {
 function preImg(fileId,imgId) {
     var imgPre =Id(imgId);
     imgPre.src = getFileUrl(fileId);
+    setTimeout(doRefresh,10);
+}
+
+function doRefresh() {
+    {
+
+        var c = document.getElementById("cvsMp");
+        var ctx = c.getContext("2d");
+        ctx.clearRect(0, 0, c.width, c.height);
+        var img = document.getElementById("imgPicHead");
+        var scaleN = Math.min(img.width, img.height) / 232;
+        if (scaleN != 0) {
+
+            console.log(scaleN);
+
+            ctx.scale(1 / scaleN, 1 / scaleN);
+            ctx.drawImage(img, 255 * scaleN - img.width / 2, 282 * scaleN - img.height / 2);
+
+            ctx.scale(scaleN, scaleN);
+        }
+        var img = document.getElementById("imgMpTemp");
+        ctx.drawImage(img, 0, 0);
+        ctx.fillStyle = '#03125e';
+        ctx.font = "56px 'Senty Golden Bell 新蒂金钟体'";
+        ctx.fillText(authorName, 498, 239);
+        ctx.fillStyle = '#92969c';
+        ctx.font = "bold 30px '宋体'";
+        ctx.fontStyle = "bold";
+        var txtLs = authorTxt.split("\n");
+        for (var i = 0; i < txtLs.length; i++)
+            ctx.fillText(txtLs[i], 505, 340 + 56 * i);
+    }
+    {
+        var c = document.getElementById("cvsCov");
+        var ctx = c.getContext("2d");
+        ctx.clearRect(0, 0, c.width, c.height);
+        var img = document.getElementById("imgCovBkg");
+        var scaleN = Math.min(img.width/900, img.height/500);
+        if (scaleN != 0) {
+            //console.log(scaleN);
+            ctx.scale(1 / scaleN, 1 / scaleN);
+            ctx.drawImage(img,0,0);
+            ctx.scale(scaleN, scaleN);
+        }
+        var img = document.getElementById("imgCovTemp");
+        ctx.drawImage(img, 0, 0);
+        ctx.fillStyle = '#000000';
+        var txtLs = coverTxt.split("\n");
+        var fontSz=48;
+        var yy=true;
+        while(yy) {
+            yy=false;
+            ctx.font = fontSz - (txtLs.length - 1) * 8 + "px 'Senty Golden Bell 新蒂金钟体'";
+            for (var i = 0; i < txtLs.length; i++)
+                if (ctx.measureText(txtLs[i]).width > 320) {
+                    fontSz -= 4;
+                    yy = true;
+                    break;
+                }
+        }
+        for (var i = 0; i < txtLs.length; i++)
+            ctx.fillText(txtLs[i], 570-ctx.measureText(txtLs[i]).width/2, 265-(txtLs.length-1)*4-(48-fontSz)/2-(56-(txtLs.length-1)*8)*(txtLs.length-1)/2 + 56 * i);
+    }
 }
